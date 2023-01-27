fda7fa141740c6a76e096120b75d1ce37e02433b
simone-viozzi <simoneviozzi97@gmail.com>
2023-01-27 15:29:26 +0100

    refactor
    - added logic to see if a file was already downloaded
    - if was partially downloaded, delete and restart
    - tqdm now display the correct unit and remaining time
    - added season folder support
    - improved logging

--- animeunity_downloader.py
+++ animeunity_downloader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 import argparse
 import sys
@@ -7,11 +9,10 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from tqdm import tqdm
 
+
 def parse_args():
 
-    parser = argparse.ArgumentParser(
-        description="AnimeUnity downloader"
-    )
+    parser = argparse.ArgumentParser(description="AnimeUnity downloader")
 
     parser.add_argument(
         "--url",
@@ -28,15 +29,41 @@ def parse_args():
         default="./anime",
     )
 
+    parser.add_argument(
+        "-S",
+        help="the season number",
+        type=int,
+        required=False,
+        default=1,
+    )
+
     return parser.parse_args()
+
+
+def download_episode(request, file_name, file_path, size):
+    with open(file_path, "wb") as f, tqdm(
+        position=1,
+        leave=False,
+        desc=file_name,
+        unit="B",
+        total=size,
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as pbar:
+        for chunk in request.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+                pbar.update(len(chunk))
+        
 
 
 def main():
 
     args = parse_args()
-    
+
     url = args.url
     output = args.output
+    season = args.S
 
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
@@ -48,34 +75,33 @@ def main():
         exit(0)
 
     anime = json.loads(items[0]["anime"])
-    title = anime["title"]
     title_eng = anime["title_eng"]
 
-    folder = Path(output).joinpath(f"{title_eng} ({title})").resolve().absolute()
-
+    folder = (Path(output) / title_eng / f"Season {season}").resolve().absolute()
     folder.mkdir(parents=True, exist_ok=True)
 
     episodes = json.loads(items[0]["episodes"])
 
     for episode in tqdm(episodes, position=0):
-        file_name = episode["file_name"]
-
-        if Path(folder).joinpath(file_name).exists():
-            continue
+        file_name = episode["file_name"].replace("_", " ")
+        file_path = folder / file_name
 
         link = episode["link"]
-        r = requests.get(link, stream=True)
-        with open(f"{folder}/{file_name}", "wb") as f:
-            for chunk in tqdm(
-                r.iter_content(chunk_size=1024),
-                position=1,
-                leave=True,
-                desc=file_name,
-                total=int(r.headers.get("content-length", 0)) // 1024,
-                unit="KB",
-            ):
-                if chunk:
-                    f.write(chunk)
+        with requests.get(link, stream=True) as r:
+            r.raise_for_status()
+            size = int(r.headers.get("content-length", 0))
+
+            if file_path.exists():
+                if file_path.stat().st_size != size:
+                    tqdm.write(f"[!] {file_name} partially downloaded, removing")
+                    file_path.unlink()
+                else:
+                    tqdm.write(f"[!] {file_name} already exists, skipping")
+                    continue
+
+            download_episode(r, file_name, file_path, size)
+
+            tqdm.write(f"[+] {file_name} downloaded")
 
 
 if __name__ == "__main__":
